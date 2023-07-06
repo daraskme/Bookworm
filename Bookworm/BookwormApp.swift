4da87563de420c8729780b12f1d676a584c47d09
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-07-06 13:09:43 +0900

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Hiroshi Inoue on R 5/07/06.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
