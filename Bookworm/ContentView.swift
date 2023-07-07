10744aecf15ba6361b56713e8ffcf592309bb3e3
DARASKdot <102633287+DARASKdot@users.noreply.github.com>
2023-07-07 21:54:29 +0900

    Creating a custom component with @Binding of swift 100days project 11

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -1,21 +1,34 @@
-//
-//  ContentView.swift
-//  Bookworm
-//
-//  Created by Hiroshi Inoue on R 5/07/06.
-//
 
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var rememberMe = false
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            PushButton(title: "Remember Me", isOn: $rememberMe)
+            Text(rememberMe ? "On" : "Off")
+        }
+    }
+}
+
+struct PushButton: View {
+    let title: String
+    @Binding var isOn: Bool
+
+    var onColors = [Color.red, Color.yellow]
+    var offColors = [Color(white: 0.6), Color(white: 0.4)]
+
+    var body: some View {
+        Button(title) {
+            isOn.toggle()
         }
         .padding()
+        .background(LinearGradient(gradient: Gradient(colors: isOn ? onColors : offColors), startPoint: .top, endPoint: .bottom))
+        .foregroundColor(.white)
+        .clipShape(Capsule())
+        .shadow(radius: isOn ? 0 : 5)
     }
 }
 
